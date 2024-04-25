85276a9cc6145e2fa23353000813ad1acc27068a
Panda77 <PiperZzz@users.noreply.github.com>
2024-04-25 11:02:16 -0400

    create PlayersRepository

--- src/main/java/associated/press/java/assignment/dao/PlayersRepository.java
+++ src/main/java/associated/press/java/assignment/dao/PlayersRepository.java
@@ -1,5 +1,10 @@
 package associated.press.java.assignment.dao;
 
-public class PlayersRepository {
-    
+import associated.press.java.assignment.model.Player;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlayersRepository extends JpaRepository<Player, String>{
+    List<Player> findByGenderAndLevelAndAge(String gender, int level, int age);
 }
