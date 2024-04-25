594eea0d7d373868c223d7c9d311e9e1cc4c8571
Panda77 <PiperZzz@users.noreply.github.com>
2024-04-25 14:04:39 -0400

    add deleteByName to sport

--- src/main/java/associated/press/java/assignment/dao/SportsRepository.java
+++ src/main/java/associated/press/java/assignment/dao/SportsRepository.java
@@ -3,11 +3,14 @@ package associated.press.java.assignment.dao;
 import associated.press.java.assignment.model.Sport;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface SportsRepository extends JpaRepository<Sport, String>{
+
+    Optional<Sport> findByName(String name);
     
     @Query("SELECT s FROM Sport s WHERE size(s.players) >= 2")
     List<Sport> findSportsWithMultiplePlayers();
--- src/main/java/associated/press/java/assignment/service/SportService.java
+++ src/main/java/associated/press/java/assignment/service/SportService.java
@@ -4,12 +4,14 @@ import associated.press.java.assignment.dao.SportsRepository;
 import associated.press.java.assignment.model.Sport;
 import associated.press.java.assignment.util.ModelMapper;
 import associated.press.java.assignment.dto.SportDTO;
+import associated.press.java.assignment.exception.ResourceNotFoundException;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class SportService {
@@ -32,5 +34,12 @@ public class SportService {
         return sports.stream()
                      .map(ModelMapper::mapSportToSportDTO)
                      .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void deleteSport(String name) {
+        Sport sport = sportsRepository.findByName(name)
+            .orElseThrow(() -> new ResourceNotFoundException("Sport not found with name: " + name));
+        sportsRepository.delete(sport);
     }
 }
