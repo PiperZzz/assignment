e4815bb6747f81f238137c1a8625603892a723cd
Panda77 <PiperZzz@users.noreply.github.com>
2024-04-25 13:19:31 -0400

    update PlayersRepository for findPlayersWithNoSports

--- src/main/java/associated/press/java/assignment/dao/PlayersRepository.java
+++ src/main/java/associated/press/java/assignment/dao/PlayersRepository.java
@@ -4,7 +4,12 @@ import associated.press.java.assignment.model.Player;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PlayersRepository extends JpaRepository<Player, String>{
+    
     List<Player> findByGenderAndLevelAndAge(String gender, int level, int age);
+
+    @Query("SELECT p FROM Player p WHERE p.sports IS EMPTY")
+    List<Player> findPlayersWithNoSports();
 }
