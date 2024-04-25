e05b756b27df9c8a65906946a545301357cb5005
Panda77 <PiperZzz@users.noreply.github.com>
2024-04-25 11:30:20 -0400

    create Sport Entity and update Player Entity for many-to-many

--- src/main/java/associated/press/java/assignment/model/Player.java
+++ src/main/java/associated/press/java/assignment/model/Player.java
@@ -2,12 +2,18 @@ package associated.press.java.assignment.model;
 
 import associated.press.java.assignment.enums.Gender;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.EnumType;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.JoinColumn;
 
 import lombok.Data;
 
@@ -28,4 +34,12 @@ public class Player {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Gender gender;
+
+    @ManyToMany
+    @JoinTable(
+        name = "players_sports",
+        joinColumns = @JoinColumn(name = "player_email", referencedColumnName = "email"),
+        inverseJoinColumns = @JoinColumn(name = "sport_name", referencedColumnName = "name")
+    )
+    private Set<Sport> sports = new HashSet<>();
 }
--- src/main/java/associated/press/java/assignment/model/Sport.java
+++ src/main/java/associated/press/java/assignment/model/Sport.java
@@ -1,5 +1,23 @@
 package associated.press.java.assignment.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "sports")
+@Data
 public class Sport {
-    
+
+    @Id
+    private String name;
+
+    @ManyToMany(mappedBy = "sports")
+    private Set<Player> players = new HashSet<>();
 }
