9927db31957b441f2127578230aa60877f0c02d5
Panda77 <PiperZzz@users.noreply.github.com>
2024-04-25 13:23:23 -0400

    update PlayerService with getPlayersWithNoSports

--- src/main/java/associated/press/java/assignment/services/PlayerService.java
+++ src/main/java/associated/press/java/assignment/services/PlayerService.java
@@ -1,9 +1,12 @@
 package associated.press.java.assignment.services;
 
 import associated.press.java.assignment.dao.PlayersRepository;
+import associated.press.java.assignment.dto.PlayerDTO;
 import associated.press.java.assignment.model.Player;
+import associated.press.java.assignment.util.ModelMapper;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,5 +19,13 @@ public class PlayerService {
 
     public List<Player> getPlayersByGenderLevelAndAge(String gender, int level, int age) {
         return playersRepository.findByGenderAndLevelAndAge(gender, level, age);
+    }
+
+    public List<PlayerDTO> getPlayersWithNoSports() {
+        List<Player> players = playersRepository.findPlayersWithNoSports();
+
+        return players.stream()
+                      .map(ModelMapper::mapPlayerToPlayerDTO)
+                      .collect(Collectors.toList());
     }
 }
