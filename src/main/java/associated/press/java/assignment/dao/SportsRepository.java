553022894f5bcf8e47edf985ffde53e6867c02bd
Panda77 <PiperZzz@users.noreply.github.com>
2024-04-25 11:41:31 -0400

    create SportsRepository and method for retrieving sports with multiple (>=2) associated players.

--- src/main/java/associated/press/java/assignment/dao/SportsRepository.java
+++ src/main/java/associated/press/java/assignment/dao/SportsRepository.java
@@ -1,5 +1,13 @@
 package associated.press.java.assignment.dao;
 
-public class SportsRepository {
-    
+import associated.press.java.assignment.model.Sport;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface SportsRepository extends JpaRepository<Sport, String>{
+    @Query("SELECT s FROM Sport s WHERE size(s.players) >= 2")
+    List<Sport> findSportsWithMultiplePlayers();
 }
