6dce7a17f34082dcbec6b0e8ec42267064f2e36d
Panda77 <PiperZzz@users.noreply.github.com>
2024-04-25 13:27:55 -0400

    update PlayerRestController for getPlayersWithNoSports

--- src/main/java/associated/press/java/assignment/controllers/PlayerRestController.java
+++ src/main/java/associated/press/java/assignment/controllers/PlayerRestController.java
@@ -1,5 +1,32 @@
 package associated.press.java.assignment.controllers;
 
+import associated.press.java.assignment.dto.PlayerDTO;
+
+import java.util.List;
+
+import associated.press.java.assignment.services.PlayerService;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/player")
 public class PlayerRestController {
-    
+
+    @Autowired
+    private PlayerService playerService;
+
+    @GetMapping("/no-sports")
+    public ResponseEntity<List<PlayerDTO>> getPlayersWithNoSports() {
+        List<PlayerDTO> players = playerService.getPlayersWithNoSports();
+        
+        if (players.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        
+        return ResponseEntity.ok(players);
+    }
 }
