632c95364f200fc0251223976835425c4202bac6
Panda77 <PiperZzz@users.noreply.github.com>
2024-04-25 10:39:03 -0400

    create Player entity

--- src/main/java/associated/press/java/assignment/model/Player.java
+++ src/main/java/associated/press/java/assignment/model/Player.java
@@ -1,5 +1,31 @@
 package associated.press.java.assignment.model;
 
+import associated.press.java.assignment.enums.Gender;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.EnumType;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "players") //JPA can infer the name, here is just for good practices.
+@Data
 public class Player {
     
+    @Id
+    private String eamil;
+    
+    @Column(nullable = false)
+    private int level;
+
+    @Column(nullable = false)
+    private int age;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Gender gender;
 }
