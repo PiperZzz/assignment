85d5c0c023628c6dffa5991dd09ca25b6f29b50d
Panda77 <PiperZzz@users.noreply.github.com>
2024-04-25 14:14:25 -0400

    add deleteSport to SportRestController

--- src/main/java/associated/press/java/assignment/controller/SportRestController.java
+++ src/main/java/associated/press/java/assignment/controller/SportRestController.java
@@ -1,6 +1,7 @@
 package associated.press.java.assignment.controller;
 
 import associated.press.java.assignment.dto.SportDTO;
+import associated.press.java.assignment.exception.ResourceNotFoundException;
 import associated.press.java.assignment.service.SportService;
 
 import java.util.List;
@@ -8,7 +9,9 @@ import java.util.List;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -29,6 +32,18 @@ public class SportRestController {
             return ResponseEntity.ok(sports);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("Error processing request: " + e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{name}")
+    public ResponseEntity<?> deleteSport(@PathVariable String name) {
+        try {
+            sportService.deleteSport(name);
+            return ResponseEntity.ok().build();
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body("Error processing request: " + e.getMessage());
         }
     }
 }
