66bb24148cc2e8e9543c4be9b65e7e39f1bea470
Panda77 <PiperZzz@users.noreply.github.com>
2024-04-25 11:07:55 -0400

    Init PlayerService for getPlayersByGenderLevelAndAge

--- src/main/java/associated/press/java/assignment/services/PlayerService.java
+++ src/main/java/associated/press/java/assignment/services/PlayerService.java
@@ -1,5 +1,20 @@
 package associated.press.java.assignment.services;
 
+import associated.press.java.assignment.dao.PlayersRepository;
+import associated.press.java.assignment.model.Player;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Service
 public class PlayerService {
-    
+
+    @Autowired
+    private PlayersRepository  playersRepository;
+
+    public List<Player> getPlayersByGenderLevelAndAge(String gender, int level, int age) {
+        return playersRepository.findByGenderAndLevelAndAge(gender, level, age);
+    }
 }
