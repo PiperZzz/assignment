9fffc52357f2809842df5031db1fcbfa78734256
Panda77 <PiperZzz@users.noreply.github.com>
2024-04-25 13:35:59 -0400

    update PlayersRepository with findByEmail

--- src/main/java/associated/press/java/assignment/dao/PlayersRepository.java
+++ src/main/java/associated/press/java/assignment/dao/PlayersRepository.java
@@ -2,11 +2,14 @@ package associated.press.java.assignment.dao;
 
 import associated.press.java.assignment.model.Player;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface PlayersRepository extends JpaRepository<Player, String>{
+
+    Optional<Player> findByEmail(String email);
     
     List<Player> findByGenderAndLevelAndAge(String gender, int level, int age);
 
