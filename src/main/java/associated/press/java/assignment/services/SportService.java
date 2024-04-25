09ca6e492a6b1ecabd0dc24921a01ad4b2fe6da5
Panda77 <PiperZzz@users.noreply.github.com>
2024-04-25 11:48:55 -0400

    Init SportService for getSportsWithMultiplePlayers

--- src/main/java/associated/press/java/assignment/services/SportService.java
+++ src/main/java/associated/press/java/assignment/services/SportService.java
@@ -1,5 +1,20 @@
 package associated.press.java.assignment.services;
 
+import associated.press.java.assignment.dao.SportsRepository;
+import associated.press.java.assignment.model.Sport;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SportService {
     
+    @Autowired
+    private SportsRepository sportsRepository;
+
+    public List<Sport> getSportsWithMultiplePlayers() {
+        return sportsRepository.findSportsWithMultiplePlayers();
+    }
 }
