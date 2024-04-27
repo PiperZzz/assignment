9dde41c5e3352d5ba24527f7c34466ec4fae8d0d
Panda77 <PiperZzz@users.noreply.github.com>
2024-04-26 20:20:24 -0400

    Add more test cases to PlayerServiceTests

--- db/tables/create_players.sql
+++ db/tables/create_players.sql
@@ -2,6 +2,6 @@ CREATE TABLE players (
     email VARCHAR(255) NOT NULL,
     level INT NOT NULL CHECK (level BETWEEN 1 AND 10),
     age INT NOT NULL,
-    gender ENUM('male', 'female') NOT NULL,
+    gender ENUM('MALE', 'FEMALE') NOT NULL,
     PRIMARY KEY (email)
 );
